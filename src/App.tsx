59befc080e80a83cc31b85376f47efc0d8a43351
Yip <batmanyp@foxmail.com>
2026-03-10 22:54:30 +0800

    feat: 第一次引入antd安装等等

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,23 @@
+// 修改 App.tsx
 import React from "react";
 import logo from "./logo.svg";
 import style from "./App.module.css";
+import { Layout, Typography, Input } from 'antd'
+const { Header } = Layout
 
 function App() {
   return (
     <div className={style.App}>
-      <header className={style["App-header"]}>
-        <img src={logo} className={style["App-log"]} alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          href="https://reactjs.org"
-          className={style["App-link"]}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Layout>
+        <Header className={style.Header}>
+          <img src={logo} alt="" className={style['App-logo']} />
+          <Typography.Title level={3} className={style.Title}>React 旅游网</Typography.Title>
+          <Input.Search 
+            placeholder="请输入旅游目的地、主题、或关键字" 
+            className={style.Search}
+          />
+        </Header>
+      </Layout>
     </div>
   );
 }
